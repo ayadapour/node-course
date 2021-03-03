cc69db7bf51d200dd7816fc7476e5c9ccb8ae38e
Aya Dapour <32078744+ayadapour@users.noreply.github.com>
2021-03-04 00:40:04 +0200

    Add files via upload

---
+++ task2/customer_app.js
@@ -0,0 +1,54 @@
+const { argv } = require('yargs')
+const mymethods=require('./myfunctions')
+yargs=require('yargs')
+yargs.command({
+    command:"addcustomer",
+    describe:'add new customer',
+    builder: {
+        id:{type:'number',demandOption:true},
+        name:{type:'string',demandOption:true},
+        balance:{type:'number',demandOption:true},
+        newbalance:{type:'number'}
+    },
+    handler:function(argv){
+        // console.log(`add customer,${argv}`)
+        customer={id:argv.id,
+            name:argv.name,
+            balance:argv.balance
+        }
+        mymethods.addNewcustomer(customer)
+    }
+})
+yargs.command({
+    command:"showall",
+    handler:function(){
+        mymethods.showdata()
+    }
+    
+})
+yargs.command({
+    command:"deletecustomer",
+    builder: {
+        id:{type:'number',demandOption:true}
+    },
+    handler:function(argv){
+        // console.log('delete')
+        mymethods.deleteCustomer(argv.id)
+    }
+})
+yargs.command({
+    command:"addbalance",
+    builder: {
+        id:{type:'number',demandOption:true}
+    },  
+    handler:function(argv){
+        customer={id:argv.id,
+            name:argv.name,
+            newbalance:argv.newbalance
+        }
+        mymethods.editBalance(argv.id,customer)
+    }
+})
+
+
+yargs.argv
